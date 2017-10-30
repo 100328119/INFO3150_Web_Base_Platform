4fdc079b1cce52bb7a22739e1d852d83f511c328
Patrick Yu <yfy69@gmail.com>
2017-10-30 15:48:40 -0700

    create new folder "new things" including file "testing_login.php"

---
+++ New Things/testing_login.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+  <h1>Login page- header changed by kun...Patrick</h1>
+
+  <form action = "varify.php" methd="POST">
+     User ID:<input type="text" name = "u_id" required/>
+     Password:<input type="text" name = "pass" required/>
+     <input type="submit" name ="re"/>
+   </form>
+ </body>
+ </html>
